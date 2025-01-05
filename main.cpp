73577446700e4e9ccabf0ab2acafd101088c2c66
H3X490N157 <73405139+H3X490N157@users.noreply.github.com>
2025-01-06 01:01:07 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <string>
+#include "ArraySequence.h" 
+#include "HeapSorter.h"    
+#include "ShellSorter.h"   
+#include "QuickSorter.h"   
+#include "ISorter.h"       
+#include "printer.h"
+#include "comparator.h"
+#include "tests.h"
+
+
+int main() {
+    char c = '1';
+    std::cout << "Введите 1 для запуска тестов. Введите 2 для пользовательского ввода. 3 для тестирования структур. Любая другая клавиша завершает программу.\n";
+    std::cin >> c;
+
+    while (c == '1' || c == '2' || c == '3') {
+        if (c == '1') {
+            HeapSorter<int> heapSorter;
+            ShellSorter<int> shellSorter;
+            QuickSorter<int> quickSorter;
+
+            HeapSorter<std::string> stringHeapSorter;
+            ShellSorter<std::string> stringShellSorter;
+            QuickSorter<std::string> stringQuickSorter;
+
+            testSorting(heapSorter, "HeapSort");
+            testSorting(shellSorter, "ShellSort");
+            testSorting(quickSorter, "QuickSort");
+
+            testSorting(stringHeapSorter, "HeapSort для строк");
+            testSorting(stringShellSorter, "ShellSort для строк");
+            testSorting(stringQuickSorter, "QuickSort для строк");
+
+            std::cout << "Тесты завершены.\n";
+        } else if (c == '2') {
+            char cc = '1';
+            std::cout << "Выберите тип данных: 1 - Числа, 2 - Строки. 3 - Person\n";
+            std::cin >> cc;
+
+            if (cc == '1') {
+                std::cout << "Выберите алгоритм сортировки: 1 - HeapSort, 2 - ShellSort, иначе - QuickSort.\n";
+                std::cin >> cc;
+
+                if (cc == '1') {
+                    HeapSorter<double> heapSorter;
+                    testUserSorting(heapSorter);
+                } else if (cc == '2') {
+                    ShellSorter<double> shellSorter;
+                    testUserSorting(shellSorter);
+                } else {
+                    QuickSorter<double> quickSorter;
+                    testUserSorting(quickSorter);
+                }
+            } else if (cc == '2') {
+                std::cout << "Выберите алгоритм сортировки: 1 - HeapSort, 2 - ShellSort, иначе - QuickSort.\n";
+                std::cin >> cc;
+
+                if (cc == '1') {
+                    HeapSorter<std::string> stringHeapSorter;
+                    testUserSortingString(stringHeapSorter);
+                } else if (cc == '2') {
+                    ShellSorter<std::string> stringShellSorter;
+                    testUserSortingString(stringShellSorter);
+                } else {
+                    QuickSorter<std::string> stringQuickSorter;
+                    testUserSortingString(stringQuickSorter);
+                }
+            }
+            else if (cc == '3') {
+                std::cout << "Выберите алгоритм сортировки: 1 - HeapSort, 2 - ShellSort, иначе - QuickSort.\n";
+                std::cin >> cc;
+                if (cc == '1') {
+                    HeapSorter<Person> personHeapSorter;
+                    testUserSortingPersons(personHeapSorter);
+                    }
+                else if (cc == '2') {
+                    ShellSorter<Person> personShellSorter;
+                    testUserSortingPersons(personShellSorter);
+                    } 
+                else {
+                    QuickSorter<Person> personQuickSorter;
+                    testUserSortingPersons(personQuickSorter);
+                }
+            }
+            else {
+                std::cout << "Неверный ввод. Возврат в главное меню.\n";
+            }
+        } else if (c == '3') {
+            testPersonSorting();
+        } else {
+            break;
+        }
+
+        std::cout << "Введите 1 для запуска тестов. Введите 2 для пользовательского ввода. 3 для тестирования структур. Любая другая клавиша завершает программу.\n";
+        std::cin >> c;
+    }
+
+    std::cout << "До свидания!\n";
+    return 0;
+}
---
+++ tests.h
@@ -0,0 +1,221 @@
+#include <chrono>
+#include <iomanip>
+#include "person.h"
+#include "csv_writer.h"
+
+
+template <typename T>
+void checkSorted(const Sequence<T>* seq) {
+    for (int i = 1; i < seq->GetLength(); ++i) {
+        assert(seq->Get(i-1) <= seq->Get(i) && "Ошибка сортировки!");
+    }
+    std::cout << "Успешно отсортировано\n";
+}
+
+
+template <typename T>
+void testSorting(ISorter<T>& sorter, const char* sorterName) {
+    std::cout << "Проверка " << sorterName << "...\n";
+
+    ArraySequence<T> emptySequence;
+    sorter.Sort(&emptySequence, compare);
+    checkSorted(&emptySequence);
+    printSequence(&emptySequence);
+
+    ArraySequence<T> singleElementSequence;
+    singleElementSequence.Append(42);
+    sorter.Sort(&singleElementSequence, compare);
+    checkSorted(&singleElementSequence);
+    printSequence(&singleElementSequence);
+
+    ArraySequence<T> sortedSequence;
+    sortedSequence.Append(1);
+    sortedSequence.Append(2);
+    sortedSequence.Append(3);
+    sorter.Sort(&sortedSequence, compare);
+    checkSorted(&sortedSequence);
+    printSequence(&sortedSequence);
+
+    ArraySequence<T> reversedSequence;
+    reversedSequence.Append(3);
+    reversedSequence.Append(2);
+    reversedSequence.Append(1);
+    sorter.Sort(&reversedSequence, compare);
+    checkSorted(&reversedSequence);
+    printSequence(&reversedSequence);
+
+    ArraySequence<T> randomSequence;
+    randomSequence.Append(10);
+    randomSequence.Append(2);
+    randomSequence.Append(8);
+    randomSequence.Append(5);
+    randomSequence.Append(3);
+    randomSequence.Append(-2281337);
+    sorter.Sort(&randomSequence, compare);
+    checkSorted(&randomSequence);
+    printSequence(&randomSequence);
+
+}
+
+
+void testSorting(ISorter<std::string>& sorter, const char* sorterName) {
+    std::cout << "Testing " << sorterName << " with strings...\n";
+
+    ArraySequence<std::string> emptySequence;
+    sorter.Sort(&emptySequence, compare<std::string>);
+    checkSorted(&emptySequence);
+    printSequence(&emptySequence);
+
+    ArraySequence<std::string> singleElementSequence;
+    singleElementSequence.Append("I am alone. HELLp");
+    sorter.Sort(&singleElementSequence, compare<std::string>);
+    checkSorted(&singleElementSequence);
+    printSequence(&singleElementSequence);
+
+    ArraySequence<std::string> reversedSequence;
+    reversedSequence.Append("Cherry");
+    reversedSequence.Append("Banana");
+    reversedSequence.Append("Apple");
+    sorter.Sort(&reversedSequence, compare<std::string>);
+    checkSorted(&reversedSequence);
+    printSequence(&reversedSequence);
+
+    ArraySequence<std::string> randomSequence;
+    randomSequence.Append("Zebra");
+    randomSequence.Append("Apple");
+    randomSequence.Append("Orange");
+    randomSequence.Append("Banana");
+    randomSequence.Append("Lemon");
+    randomSequence.Append("Берёзовый сок");
+    randomSequence.Append("Я не люблю BTS");
+    sorter.Sort(&randomSequence, compare<std::string>);
+    checkSorted(&randomSequence);
+    printSequence(&randomSequence);
+}
+
+
+template <typename T>
+void testUserSorting(ISorter<T>& sorter) {
+    ArraySequence<T> sequence_test;
+    T x;
+
+    std::cout << "Введите элементы последовательности (введите 0 для завершения ввода):\n";
+    while (true) {
+        std::cin >> x;
+        if (x == 0) break;
+        sequence_test.Append(x);
+    }
+
+    std::cout << "Исходная последовательность:\n";
+    printSequence(&sequence_test);
+
+    sorter.Sort(&sequence_test, compare<T>);
+    std::cout << "Отсортированная последовательность:\n";
+    printSequence(&sequence_test);
+
+    checkSorted(&sequence_test);
+}
+
+void testUserSortingString(ISorter<std::string>& sorter) {
+    ArraySequence<std::string> sequence_test;
+    std::string input;
+
+    std::cout << "Введите строки для сортировки (введите пустую строку для завершения ввода):\n";
+    std::cin.ignore(); // Очистка потока
+    while (true) {
+        std::getline(std::cin, input);
+        if (input.empty()) break;
+        sequence_test.Append(input);
+    }
+
+    std::cout << "Исходная последовательность:\n";
+    printSequence(&sequence_test);
+
+    sorter.Sort(&sequence_test, compare<std::string>);
+    std::cout << "Отсортированная последовательность:\n";
+    printSequence(&sequence_test);
+
+    checkSorted(&sequence_test);
+}
+
+template <typename T>
+void measureSortingTime(ISorter<T>& sorter, ArraySequence<T>& sequence, const std::string& algorithmName) {
+    auto start = std::chrono::high_resolution_clock::now();
+    sorter.Sort(&sequence, comparePersons);  // Сортировка
+    auto end = std::chrono::high_resolution_clock::now();
+    
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << algorithmName << " завершен за " << duration.count() << " миллисекунд.\n";
+}
+
+void testPersonSorting() {
+    std::vector<Person> persons = readPersonsFromFile("persons.csv");
+    if (persons.empty()) {
+        std::cerr << "Нет данных для теста." << std::endl;
+        return;
+    }
+
+    ArraySequence<Person> personSequence;
+    for (const auto& person : persons) {
+        personSequence.Append(person);
+    }
+
+    HeapSorter<Person> heapSorter;
+    ShellSorter<Person> shellSorter;
+    QuickSorter<Person> quickSorter;
+
+
+    ArraySequence<Person> personSequenceForHeapSort = personSequence;  
+    measureSortingTime(heapSorter, personSequenceForHeapSort, "HeapSort");
+
+    ArraySequence<Person> personSequenceForShellSort = personSequence;  
+    measureSortingTime(shellSorter, personSequenceForShellSort, "ShellSort");
+
+    ArraySequence<Person> personSequenceForQuickSort = personSequence; 
+    measureSortingTime(quickSorter, personSequenceForQuickSort, "QuickSort");
+
+    writeResultsToCSV(personSequenceForQuickSort, "results.csv");
+
+    std::cout << "Тестирование завершено.\n";
+}
+
+void testUserSortingPersons(ISorter<Person>& sorter) {
+    std::cout << "Выберите способ ввода данных: (1 - вручную, 2 - файл): ";
+    int inputChoice;
+    std::cin >> inputChoice;
+    ArraySequence<Person> persons;
+    if (inputChoice == 1) {
+        std::cout << "Введите количество записей: ";
+        int n;
+        std::cin >> n;
+        std::cout << "Вводите данные в формате: Имя Фамилия Год_Рождения Рост Вес\n";
+        while (n--) {
+            std::string firstName, lastName;
+            int birthYear;
+            double height, weight;
+            std::cin >> firstName >> lastName >> birthYear >> height >> weight;
+            persons.Append({firstName, lastName, birthYear, height, weight});
+        }
+    } else {
+        std::cout << "Введите имя файла с данными о людях (CSV: Имя,Фамилия,ГодРождения,Рост,Вес): ";
+        std::string filename;
+        std::cin >> filename;
+        std::vector<Person> vectorPersons = readPersonsFromFile(filename);
+        for (const auto& person : vectorPersons) {
+            persons.Append(person);
+        }
+    }
+    std::cout << "Сортировка началась...\n";
+    sorter.Sort(&persons, comparePersons);
+
+    // Вывод результата
+    std::cout << "Сортировка завершена. Результат:\n";
+    for (int i = 0; i < persons.GetLength(); ++i) {
+        const Person& person = persons.Get(i);
+        std::cout << person.GetFirstName() << " "
+                  << person.GetLastName() << ", "
+                  << person.GetBirthYear() << ", "
+                  << person.GetHeight() << "m, "
+                  << person.GetWeight() << "kg" << std::endl;
+    }
+}
